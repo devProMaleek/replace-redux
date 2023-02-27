51053549b266260a40c4f8f04ebc5d99d0239a2d
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-02-27 22:40:19 +0100

    feat: Getting Started with a Custom Hook as a Store

---
+++ src/hooks-store/store.js
@@ -0,0 +1,18 @@
+import { useState } from "react"
+
+let globalState = {}
+let listeners = []
+let actions = {}
+
+const useStore = () => {
+  const setState = useState(globalState)[1];
+
+  useEffect(() => {
+    listeners.push(setState);
+  
+    return () => {
+      listeners = listeners.filter(listener => listener !== setState);
+    }
+  }, [])
+  
+}
