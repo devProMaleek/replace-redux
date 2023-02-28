605999de1f0b4035e7efae81cf8c2a5fafae8c17
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-02-28 08:55:03 +0100

    feat: Optimize the custom hook

--- src/hooks-store/store.js
+++ src/hooks-store/store.js
@@ -1,38 +1,41 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from 'react';
 
-let globalState = {}
-let listeners = []
-let actions = {}
+let globalState = {};
+let listeners = [];
+let actions = {};
 
-const useStore = () => {
+const useStore = (shouldListen = true) => {
   const setState = useState(globalState)[1];
 
   const dispatch = (actionIdentifiers, payload) => {
     const newState = actions[actionIdentifiers](globalState, payload);
-    globalState = {...globalState, ...newState};
+    globalState = { ...globalState, ...newState };
 
     for (const listener of listeners) {
       listener(globalState);
     }
-  }
+  };
 
   useEffect(() => {
-    listeners.push(setState);
-  
-    return () => {
-      listeners = listeners.filter(listener => listener !== setState);
+    if (shouldListen) { 
+      listeners.push(setState);
     }
-  }, [setState])
 
-  return [globalState, dispatch]; 
-  
-}
+    return () => {
+      if (shouldListen) {
+        listeners = listeners.filter((listener) => listener !== setState);
+      }
+    };
+  }, [setState, shouldListen]);
+
+  return [globalState, dispatch];
+};
 
 export const initStore = (userActions, initialState) => {
   if (initialState) {
-    globalState = {...globalState, ...initialState};
+    globalState = { ...globalState, ...initialState };
   }
-  actions = {...actions, ...userActions};
-}
+  actions = { ...actions, ...userActions };
+};
 
 export default useStore;
